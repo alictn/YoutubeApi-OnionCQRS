eb71176377395c67bf4ff158d86284c76814e287
alictn <alictn_ky@outlook.com>
2025-05-14 11:18:57 +0300

    Write Repository Geliştirmesi Yapıldı

--- YoutubeApi.Persistence/Repositories/WriteRepositories.cs
+++ YoutubeApi.Persistence/Repositories/WriteRepositories.cs
@@ -1,6 +1,39 @@
-﻿namespace YoutubeApi.Persistence.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using YoutubeApi.Application.Interfaces.Repositories;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Persistence.Repositories
 {
-    public class WriteRepositories
+    public class WriteRepositories<T> : IWriteRepositories<T> where T : class, IEntityBase, new()
     {
+        private readonly DbContext dbContext;
+
+        public WriteRepositories(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+        }
     }
 }
